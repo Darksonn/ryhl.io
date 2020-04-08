b5d1a46c586645f2306232e1a35c0e6a0840ce7a
Alice Ryhl <alice@ryhl.io>
2020-04-08 21:32:59 +0200

    Fix webp images

--- publish.sh
+++ publish.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
 zola build
+
+for f in $(find public -iname '*.png'); do
+  convert "$f" -define webp:lossless=true "$f.webp"
+done
+for f in $(find public -iname '*.jpg'); do
+  convert "$f" "$f.webp"
+done
+
 rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/html
 
