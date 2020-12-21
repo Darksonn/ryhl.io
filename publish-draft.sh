274927edbeeb71da4dd343d36f67ede152f7eb97
Alice Ryhl <alice@ryhl.io>
2020-12-21 11:41:45 +0100

    Disallow indexing on draft subdomain

--- publish-draft.sh
+++ publish-draft.sh
@@ -6,5 +6,6 @@ sed -i '/base_url/s/ryhl/draft.ryhl/' config.toml
 zola build --drafts
 mv /tmp/config.toml config.toml
 
+cp ./draft-robots.txt ./public/robots.txt
 rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/draft
 
