9ea03d521ccf5374da9c9255d479da640a3bde8e
Alice Ryhl <alice@ryhl.io>
2020-12-21 12:04:27 +0100

    Add noindex to draft

--- publish-draft.sh
+++ publish-draft.sh
@@ -7,5 +7,6 @@ zola build --drafts
 mv /tmp/config.toml config.toml
 
 cp ./draft-robots.txt ./public/robots.txt
+find ./public -type f -exec sed -i 's/<meta name="robots" content="index,follow">/<meta name="robots" content="noindex">/' {} \;
 rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/draft
 
