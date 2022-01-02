eeafc4a786ee1c6e2d2780e63628f7d2da22ad00
Alice Ryhl <aliceryhl@google.com>
2022-01-02 11:08:15 +0100

    Fix publish-draft.sh

--- publish-draft.sh
+++ publish-draft.sh
@@ -7,6 +7,8 @@ zola build --drafts
 mv /tmp/config.toml config.toml
 
 cp ./draft-robots.txt ./public/robots.txt
+
+sed -i 's!<th><code>&amp;mut T</code></th>!<th><code>\&amp;mut\&nbsp;T</code></th>!' ./public/blog/temporary-shared-mutation/index.html
 find ./public -type f -exec sed -i 's/<meta name="robots" content="index,follow">/<meta name="robots" content="noindex">/' {} \;
-rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/draft
+rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/draft.ryhl.io
 
