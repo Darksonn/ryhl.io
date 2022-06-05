3a4c9ee44eb674ced4bee395079f0129710d05d1
Alice Ryhl <aliceryhl@google.com>
2022-06-05 14:55:03 +0200

    Add static compression

--- publish.sh
+++ publish.sh
@@ -12,4 +12,6 @@ done
 sed -i 's!<th><code>&amp;mut T</code></th>!<th><code>\&amp;mut\&nbsp;T</code></th>!' ./public/blog/temporary-shared-mutation/index.html
 
 rsync -avz --delete ./public/* ./public/.well-known nine:/var/www/ryhl.io
-
+ssh nine find /var/www/ryhl.io -name '*.gz' -exec rm -f -- '{}' '\;'
+ssh nine find /var/www/ryhl.io '\(' -name '*.html' -o -name '*.css' -o -name '*.xml' -o -name '*.ico' -o -name '*.svg' -o -name '*.json' '\)' -exec gzip -9fk -- '{}' '\;'
+# html,css,xml,ico,svg,json
